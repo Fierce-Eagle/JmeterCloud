4785d9c265a09682b38d020cf297cff7dfc48a22
Fierce-Eagle <rukin_nikitka@mail.ru>
2021-08-25 09:08:56 +0700

    add comments

---
+++ src/test/java/com/jmeterPractice/practice/model/JmeterTestingTest.java
@@ -0,0 +1,11 @@
+package com.jmeterPractice.practice.model;
+
+import org.junit.Test;
+
+public class JmeterTestingTest {
+    @Test
+    public void start() {
+        JmeterTesting.getJmeterTest().start();
+    }
+
+}
