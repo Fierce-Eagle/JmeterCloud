11cfb68e7108add9d904696c097570f3384dc413
MrsDramaqueen <chelyaev_99@mail.ru>
2021-08-25 18:31:58 +0700

    rename

--- src/main/java/com/jmeterpractice/practice/controllers/MainController.java
+++ src/main/java/com/jmeterpractice/practice/controllers/MainController.java
@@ -1,6 +1,6 @@
 package com.jmeterpractice.practice.controllers;
 
-import com.jmeterpractice.practice.helpclasses.JmeterTesting;
+import com.jmeterpractice.practice.modules.JmeterTesting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/test/java/com/jmeterpractice/practice/modules/JmeterTestingTest.java
@@ -0,0 +1,12 @@
+package com.jmeterpractice.practice.modules;
+
+import org.junit.Test;
+
+public class JmeterTestingTest {
+
+    @Test
+    public void start() {
+        JmeterTesting.getJmeterTest().start();
+    }
+
+}
