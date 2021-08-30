ad7e2a332808c4922953fa0a59544b1ad48b45b8
rukin <rukin_nikitka@mail.ru>
2021-08-30 21:44:04 +0700

    add part of task

--- src/main/java/com/jmeterpractice/practice/modules/JmeterTesting.java
+++ src/main/java/com/jmeterpractice/practice/modules/JmeterTesting.java
@@ -4,9 +4,12 @@ import com.blazemeter.jmeter.threads.concurrency.ConcurrencyThreadGroup;
 import org.apache.jmeter.control.LoopController;
 import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
+import org.apache.jmeter.reporters.ResultCollector;
+import org.apache.jmeter.reporters.Summariser;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.testelement.TestPlan;
 import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jmeter.visualizers.BeanShellListener;
 import org.apache.jorphan.collections.HashTree;
 
 public class JmeterTesting {
@@ -73,6 +76,9 @@ public class JmeterTesting {
         loopController.setProperty(TestElement.TEST_CLASS, LoopController.class.getName());
 
         // add info about listeners
+        //ResultCollector
+        //Summariser
+        SummariserR
 
         // ConcurrencyThreadGroup
         ConcurrencyThreadGroup threadGroup = new ConcurrencyThreadGroup();
